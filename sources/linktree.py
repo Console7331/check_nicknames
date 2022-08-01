fa598fbc38928af363ab76a73d9e98ce574b907e
Roman <37276774+Console7331@users.noreply.github.com>
2022-08-01 13:46:21 +0300

    first init

---
+++ sources/linktree.py
@@ -0,0 +1,21 @@
+import requests
+
+url = "https://linktr.ee/validate/username"
+name = "linktree"
+payload={'username': 'console7331'}
+files=[
+
+]
+headers = {
+  'Cookie': 'countryCode=RU'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload, files=files)
+
+resoult = []
+
+
+#print(response.text)
+#if response.text == {"result":"success"} : {
+#  resoult.extend()
+#}
