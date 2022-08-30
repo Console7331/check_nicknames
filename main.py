18516a4a6066ea520b45e2a2ca3e984fe0a752cd
Roman <37276774+Console7331@users.noreply.github.com>
2022-08-30 14:57:05 +0300

    if name == main

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 import os
 
-os.system('cls' if os.name=='nt' else 'clear')
-nickname = input("Please, type your nickname here: ")
+def main():
+    os.system('cls' if os.name=='nt' else 'clear')
+    nickname = input("Please, type your nickname here: ")
 
-if nickname is None:
-		print ("Please type your nickname")
-		os._exit(0)
+    if nickname is None:
+            print ("Please type your nickname")
+            os._exit(0)
 
-f = open('output.txt', 'w').close()
+    f = open('output.txt', 'w').close()
 
-email_check = input("Do you wanna check emails? y/n: ")
-if (email_check == 'y'):
-    from sources import emails
-    emails.check_emails(nickname)
+    email_check = input("Do you wanna check emails? y/n: ")
+    if (email_check == 'y'):
+        from sources import emails
+        emails.check_emails(nickname)
 
-services_check = input("Do you wanna check services? y/n: ")
-if (services_check == 'y'):
-    from sources import services
-    services.check_services(nickname)
-if (services_check == 'y') or (email_check == 'y'):
-    print("Check output.txt file, there is list of all available emails and services")
+    services_check = input("Do you wanna check services? y/n: ")
+    if (services_check == 'y'):
+        from sources import services
+        services.check_services(nickname)
+    if (services_check == 'y') or (email_check == 'y'):
+        print("Check output.txt file, there is list of all available emails and services")
+
+print(__name__)
+if __name__ == "__main__":
+	main()
