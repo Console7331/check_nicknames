9e025a926d7eb678ed9e43cf547b561ebac2c81f
Roman <37276774+Console7331@users.noreply.github.com>
2022-09-02 11:17:18 +0300

    fixed logic

--- sources/services/habr.py
+++ sources/services/habr.py
@@ -6,10 +6,12 @@ def habr_check(nickname, service_list):
     url = "https://habr.com/ru/users/"+nickname
     payload={}
     headers = {}
-    response = requests.get(url, headers=headers, data=payload).text
-    resp_dict = {'0': color('red')+'[-] Nickname is already use!'+color('end'), '1': color('green')+'[+] Nickname is available!'+color('end')}
-    if '<h1 class="tm-error-message__title">' in response:
+    response = requests.get(url, headers=headers, data=payload).status_code
+    resp_dict = {'0': color('red')+'[-] Nickname is already use!'+color('end'), '1': color('green')+'[+] Nickname is available!'+color('end'), '2': color('red')+'[-] Nickname is invalid!'+color('end')}
+    if response == '404':
         print(resp_dict.get('1'), 'Service: Habr')
         service_list.append('Habr')
-    else:
+    elif response == '200':
         print(resp_dict.get('0'), 'Service: Habr')
+    else: 
+        print(resp_dict.get('2'), 'Service: Habr')
