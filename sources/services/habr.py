628aec8003f74be0d837a187984edc43229842bb
Roman <37276774+Console7331@users.noreply.github.com>
2022-09-02 11:20:07 +0300

    bug fix

--- sources/services/habr.py
+++ sources/services/habr.py
@@ -8,10 +8,10 @@ def habr_check(nickname, service_list):
     headers = {}
     response = requests.get(url, headers=headers, data=payload).status_code
     resp_dict = {'0': color('red')+'[-] Nickname is already use!'+color('end'), '1': color('green')+'[+] Nickname is available!'+color('end'), '2': color('red')+'[-] Nickname is invalid!'+color('end')}
-    if response == '404':
+    if response == 404:
         print(resp_dict.get('1'), 'Service: Habr')
         service_list.append('Habr')
-    elif response == '200':
+    elif response == 200:
         print(resp_dict.get('0'), 'Service: Habr')
     else: 
         print(resp_dict.get('2'), 'Service: Habr')
