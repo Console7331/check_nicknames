67117c3c6b34b4846f6eccd946c89ec0de12a49b
Roman <37276774+Console7331@users.noreply.github.com>
2022-09-08 17:29:18 +0300

    fixed bug at instagram

--- sources/services/instagram.py
+++ sources/services/instagram.py
@@ -11,13 +11,15 @@ def instagram_check(nickname, service_list):
     instagram_cookie = response_sess.cookies.get_dict()
     headers['x-csrftoken'] = instagram_cookie.get('csrftoken')
     url = "https://www.instagram.com/accounts/web_create_ajax/attempt/"
-    payload='username='+nickname
+    payload = ('username='+nickname)
     headers['Content-Type'] = 'application/x-www-form-urlencoded'
     headers['user-agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.167 YaBrowser/22.7.4.957 Yowser/2.5 Safari/537.36'
     response = requests.post(url, headers=headers, data=payload, cookies=instagram_cookie)
-    resp_dict = {'0': color('red')+'[-] Nickname is already use!'+color('end'), '1': color('green')+'[+] Nickname is available!'+color('end')}
-    if b'"code": "username_is_taken"' in response:
+    resp_dict = {'0': color('red')+'[-] Nickname is already use!'+color('end'), '1': color('green')+'[+] Nickname is available!'+color('end'), '2': color('red')+'[-] Nickname is invalid!'+color('end')}
+    if '"code": "username_is_taken"' in response.text:
         print(resp_dict.get('0'), 'Service: Instagram')
+    elif response.status_code != 200:
+        print(resp_dict.get('2'), 'Service: Instagram')
     else:
         print(resp_dict.get('1'), 'Service: Instagram')
         service_list.append('Instagram')
